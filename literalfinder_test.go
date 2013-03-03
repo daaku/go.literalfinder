5c825eb066eae2683d29bd5b1f36b58aee65fca3
Naitik Shah <n@daaku.org>
2013-03-03 02:21:48 -0800

    a few more tests

--- literalfinder_test.go
+++ literalfinder_test.go
@@ -5,7 +5,24 @@ import (
 	"testing"
 )
 
-func TestSimpleFind(t *testing.T) {
+func TestNoLiterals(t *testing.T) {
+	t.Parallel()
+	const source = `
+  package foo
+  type Foo struct {
+    Bar bool
+  }
+  `
+	i, err := literalfinder.Find("Foo", "foo.go", source)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(i) != 0 {
+		t.Fatal("was expecting 0 instance")
+	}
+}
+
+func TestSingleString(t *testing.T) {
 	t.Parallel()
 	const source = `
   package foo
@@ -23,5 +40,30 @@ func TestSimpleFind(t *testing.T) {
 	}
 	if v := i[0].Fields["Bar"]; v != "one" {
 		t.Fatalf("was expecting one got %s", v)
+	}
+}
+
+func TestBools(t *testing.T) {
+	t.Parallel()
+	const source = `
+  package foo
+  type Foo struct {
+    Bar bool
+  }
+  var f = &Foo{Bar: true}
+  var g = &Foo{Bar: false}
+  `
+	i, err := literalfinder.Find("Foo", "foo.go", source)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(i) != 2 {
+		t.Fatal("was expecting 2 instance")
+	}
+	if v := i[0].Fields["Bar"]; v != true {
+		t.Fatalf("was expecting true got %s", v)
+	}
+	if v := i[1].Fields["Bar"]; v != false {
+		t.Fatalf("was expecting false got %s", v)
 	}
 }

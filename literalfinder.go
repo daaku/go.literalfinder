d878f4446e4cf60a149e1b99da6f1b9500750ad2
Naitik Shah <n@daaku.org>
2013-03-09 11:12:47 -0800

    docs

--- literalfinder.go
+++ literalfinder.go
@@ -1,5 +1,12 @@
 // Package literalfinder helps find literals in source creating instances of a
 // specified struct.
+//
+// This library uses the go/types library available in Go 1.1. The basic
+// process is to create a Finder instance, add files from a package and find
+// the contained literals. The API disallows non-literal instances. It
+// currently also disallows positional initialization, though this may be
+// allowed in the future. Additionally the struct may currently only contain
+// basic literal values (bool, string, int64).
 package literalfinder
 
 import (
@@ -17,12 +24,18 @@ var (
 	errMustUseKeyValueSyntax = errors.New("must use key value value syntax")
 )
 
+// A Finder instance allows accumulating source files in a given package, thru
+// which one can eventually find literal instances.
 type Finder struct {
 	thing   string
 	fileSet *token.FileSet
 	files   []*ast.File
 }
 
+// Create a new Finder instance. "thing" should be a fully qualified reference,
+// like "github.com/go.pkgrsrc/pkgrsrc.Config". In that example,
+// "github.com/go.pkgrsrc/pkgrsrc" is the package import path, and "Config" is
+// the name of the struct type.
 func NewFinder(thing string) *Finder {
 	return &Finder{
 		fileSet: token.NewFileSet(),
@@ -30,6 +43,9 @@ func NewFinder(thing string) *Finder {
 	}
 }
 
+// Add a file from the package being processed. The src argument can be a
+// string, []byte or io.Reader. If src == nil, the source will be read from the
+// specified filename.
 func (f *Finder) Add(filename string, src interface{}) error {
 	astf, err := parser.ParseFile(f.fileSet, filename, src, parser.ParseComments)
 	if err != nil {
@@ -39,6 +55,8 @@ func (f *Finder) Add(filename string, src interface{}) error {
 	return nil
 }
 
+// Find the instances and populate into. into should be an slice of the struct
+// type that is being found.
 func (f *Finder) Find(into interface{}) error {
 	var instances []map[string]interface{}
 	var retErr error
